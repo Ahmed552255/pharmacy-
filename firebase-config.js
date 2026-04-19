8039d98fe1352473ac11f98f6099d8f302ff1820
Ahmed Mohammed Suleiman <ahmed2217p@gmail.com>
2026-04-19 08:33:42 -0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+// إعدادات Firebase لمشروع "the-final-e012c"
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyBg0CcRmfjE_AFGHa4miGEJ5ffTKDPuK-Y",
+    authDomain: "the-final-e012c.firebaseapp.com",
+    databaseURL: "https://the-final-e012c-default-rtdb.firebaseio.com",
+    projectId: "the-final-e012c",
+    storageBucket: "the-final-e012c.firebasestorage.app",
+    messagingSenderId: "291006056527",
+    appId: "1:291006056527:web:660f1b24d984b276e3c23a",
+    measurementId: "G-DSSNFXDKJJ"
+};
