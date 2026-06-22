0515bdc3350f951bd1b6aad8cb4a77c80270699f
Ahmed Mohammed Suleiman <ahmed2217p@gmail.com>
2026-06-22 06:07:00 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,13 @@
 // firebase-config.js
-// إعدادات Firebase لمشروع "i-excelled"
+// إعدادات Firebase لمشروع "شفاء"
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyCjCLdv6QWfJieZQO7Qt_U5W0UgxNx8TXQ",
-    authDomain: "i-excelled.firebaseapp.com",
-    databaseURL: "https://i-excelled-default-rtdb.firebaseio.com",
-    projectId: "i-excelled",
-    storageBucket: "i-excelled.firebasestorage.app",
-    messagingSenderId: "872523261091",
-    appId: "1:872523261091:web:77f465166d877b96b84b16",
-    measurementId: "G-DJBW60TWCF"
+    apiKey: "AIzaSyBwU26WBqYZ728pV4sBdQa09OOpP4GYMsE",
+    authDomain: "shifa-51011.firebaseapp.com",
+    databaseURL: "https://shifa-51011-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "shifa-51011",
+    storageBucket: "shifa-51011.firebasestorage.app",
+    messagingSenderId: "184548486784",
+    appId: "1:184548486784:web:c9941b183af58b9eca8065",
+    measurementId: "G-1NJH9YQT4G"
 };
