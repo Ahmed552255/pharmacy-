2e33d7ef25a3928f9bef66e46ad205dcca00be28
Ahmed Mohammed Suleiman <ahmed2217p@gmail.com>
2026-07-07 08:34:15 +0300

    Revise Firebase config for pharmacy project
    Updated Firebase configuration for the pharmacy project, including new API keys and initialization of services.

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,32 @@
-// firebase-config.js
-// إعدادات Firebase لمشروع "شفاء"
+<script type="module">
+  // استيراد الدوال المطلوبة من مكتبات Firebase
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-analytics.js";
+  import { getDatabase } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-database.js";
+  import { getAuth } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+  import { getStorage } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-storage.js";
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-analytics.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-database.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-storage.js";
+  // إعدادات Firebase الخاصة بمشروع "pharmacy"
+  const firebaseConfig = {
+    apiKey: "AIzaSyDpauwBjKHzaRC0Rw87W_-tZaEnGhONzOc",
+    authDomain: "pharmacy-8bc6b.firebaseapp.com",
+    databaseURL: "https://pharmacy-8bc6b-default-rtdb.firebaseio.com",
+    projectId: "pharmacy-8bc6b",
+    storageBucket: "pharmacy-8bc6b.firebasestorage.app",
+    messagingSenderId: "718471229146",
+    appId: "1:718471229146:web:2a524ae736817b66ccbfe3",
+    measurementId: "G-S4ZMPLD557"
+  };
 
-// إعدادات Firebase
-export const firebaseConfig = {
-    apiKey: "AIzaSyAfYqB_0OcQcnYvxP6C0J4cuViY6EmLE8U",
-    authDomain: "fast-sokon.firebaseapp.com",
-    databaseURL: "https://fast-sokon-default-rtdb.firebaseio.com",
-    projectId: "fast-sokon",
-    storageBucket: "fast-sokon.firebasestorage.app",
-    messagingSenderId: "45959507911",
-    appId: "1:45959507911:web:e78de78f4a928062dff3e2",
-    measurementId: "G-2R52W7Q4CE"
-};
+  // تهيئة Firebase
+  const app = initializeApp(firebaseConfig);
+  
+  // تهيئة الخدمات
+  const analytics = getAnalytics(app);
+  const database = getDatabase(app);
+  const auth = getAuth(app);
+  const storage = getStorage(app);
 
+  // يمكنك تصدير هذه الثوابت لاستخدامها في ملفات أخرى إذا لزم الأمر
+  export { app, analytics, database, auth, storage };
+</script>
