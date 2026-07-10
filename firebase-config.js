076b7f11b81941f7a91c3e11d3f0e0e691c37899
Ahmed Mohammed Suleiman <ahmed2217p@gmail.com>
2026-07-10 18:57:47 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,32 +1,19 @@
-<script type="module">
-  // استيراد الدوال المطلوبة من مكتبات Firebase
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-analytics.js";
-  import { getDatabase } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-database.js";
-  import { getAuth } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
-  import { getStorage } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-storage.js";
+// ملف تكوين Firebase المنفصل
+// firebase-config.js
 
-  // إعدادات Firebase الخاصة بمشروع "pharmacy"
-  const firebaseConfig = {
-    apiKey: "AIzaSyDpauwBjKHzaRC0Rw87W_-tZaEnGhONzOc",
-    authDomain: "pharmacy-8bc6b.firebaseapp.com",
-    databaseURL: "https://pharmacy-8bc6b-default-rtdb.firebaseio.com",
-    projectId: "pharmacy-8bc6b",
-    storageBucket: "pharmacy-8bc6b.firebasestorage.app",
-    messagingSenderId: "718471229146",
-    appId: "1:718471229146:web:2a524ae736817b66ccbfe3",
-    measurementId: "G-S4ZMPLD557"
-  };
+const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDosyySkPDI-uCQzHOk7jTDUFp2U2jQAzo",
+    authDomain: "i-excelled.firebaseapp.com",
+    databaseURL: "https://i-excelled-default-rtdb.firebaseio.com",
+    projectId: "i-excelled",
+    storageBucket: "i-excelled.firebasestorage.app",
+    messagingSenderId: "872523261091",
+    appId: "1:872523261091:web:0d8bb389f377752cb84b16",
+    measurementId: "G-DLN1FG2Y4D"
+};
 
-  // تهيئة Firebase
-  const app = initializeApp(firebaseConfig);
-  
-  // تهيئة الخدمات
-  const analytics = getAnalytics(app);
-  const database = getDatabase(app);
-  const auth = getAuth(app);
-  const storage = getStorage(app);
+// تعريض التكوين للمتغير العام
+window.FIREBASE_CONFIG = FIREBASE_CONFIG;
 
-  // يمكنك تصدير هذه الثوابت لاستخدامها في ملفات أخرى إذا لزم الأمر
-  export { app, analytics, database, auth, storage };
-</script>
+// يمكنك أيضاً تصديره إذا كنت تستخدم ES modules
+export default FIREBASE_CONFIG;
